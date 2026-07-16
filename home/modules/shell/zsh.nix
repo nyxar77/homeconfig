66a6501bc7d853442f87ed96bc5e978e33fc06f5
fe2-Nyxar <nyxar49@gmail.com>
2026-07-16 22:45:02 +0100

    improve zsh navigation and highlighting

--- home/modules/shell/zsh.nix
+++ home/modules/shell/zsh.nix
@@ -1,10 +1,22 @@
 {pkgs, ...}: {
   programs.autojump.enable = true;
+  programs.zoxide = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+  programs.sesh = {
+    enable = true;
+    tmuxKey = "s";
+  };
   programs.fzf = {
     enable = true;
     enableZshIntegration = true;
-    fileWidgetCommand = "fd --type f"; #ATT: for file search <C-T>
-    fileWidgetOptions = ["--color='bg:,bg+:,spinner:#e965a5,hl:#b1baf4,fg:#eee9fc,header:#e965a5,info:#e965a5,pointer:#e965a5,marker:#e965a5,fg+:#e965a5,prompt:#e965a5,hl+:#ebde76,selected-bg:,border:#e965a5,label:#eee9fc'" "--preview 'bat --style=numbers --color=always {}'"];
+    tmux.enableShellIntegration = true;
+    fileWidgetCommand = "fd --type f"; # ATT: for file search <C-T>
+    fileWidgetOptions = [
+      "--color='bg:,bg+:,spinner:#e965a5,hl:#b1baf4,fg:#eee9fc,header:#e965a5,info:#e965a5,pointer:#e965a5,marker:#e965a5,fg+:#e965a5,prompt:#e965a5,hl+:#ebde76,selected-bg:,border:#e965a5,label:#eee9fc'"
+      "--preview 'bat --style=numbers --color=always {}'"
+    ];
     # historyWidgetCommand = "fc -l 0"; #ATT: to search through history <C-R>
     # historyWidgetOptions = ["--sort" "--exact" "--color='bg:,bg+:,spinner:#e965a5,hl:#b1baf4,fg:#eee9fc,header:#e965a5,info:#e965a5,pointer:#e965a5,marker:#e965a5,fg+:#e965a5,prompt:#e965a5,hl+:#ebde76,selected-bg:,border:#e965a5,label:#eee9fc'"];
     colors = {
@@ -29,7 +41,25 @@
     enable = true;
     enableCompletion = true;
     autosuggestion.enable = true;
-    syntaxHighlighting.enable = true;
+    syntaxHighlighting = {
+      enable = true;
+      styles = {
+        arg0 = "fg=#b1f2a7,bold";
+        command = "fg=#b1f2a7,bold";
+        precommand = "fg=#b1f2a7,underline";
+        path = "fg=#b3f4f3,underline";
+        globbing = "fg=#b1baf4";
+        redirection = "fg=#ebde76,bold";
+        reserved-word = "fg=#ebde76,bold";
+        single-quoted-argument = "fg=#ebde76";
+        double-quoted-argument = "fg=#ebde76";
+        back-dollar-quoted-argument = "fg=#b3f4f3";
+        back-double-quoted-argument = "fg=#b3f4f3";
+        unknown-token = "fg=#e965a5,bold";
+        bracket-error = "fg=#e965a5,bold";
+        comment = "fg=#938aad,italic";
+      };
+    };
     completionInit = "autoload -U compinit && compinit";
 
     shellAliases = {
@@ -42,15 +72,16 @@
     initContent = ''
       source ${pkgs.nix-index}/etc/profile.d/command-not-found.sh
       fastfetch
-      aj() {
-          cd "$(autojump $1)"
-      }
     '';
 
     history.size = 10000;
     history.ignoreAllDups = true;
     history.path = "$HOME/.zsh_history";
-    history.ignorePatterns = ["rm *" "pkill *" "cp *"];
+    history.ignorePatterns = [
+      "rm *"
+      "pkill *"
+      "cp *"
+    ];
     plugins = [
       {
         name = "zsh-autocomplete";
