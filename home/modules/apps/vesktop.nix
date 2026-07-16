778b70a79b13f048db4f8e9f0ad60584e358d16f
fe2-Nyxar <nyxar49@gmail.com>
2026-07-16 22:40:31 +0100

    update vesktop and vencord settings

--- home/modules/apps/vesktop.nix
+++ home/modules/apps/vesktop.nix
@@ -1,45 +1,57 @@
-{unstablePkgs, ...}: {
+{...}: {
   programs.vesktop = {
     enable = true;
     # package = unstablePkgs.vesktop;
     settings = {
-      appBadge = true;
       arRPC = true;
       checkUpdates = true;
+      discordBranch = "stable";
+
+      appBadge = true;
       customTitleBar = true;
-      disableMinSize = true;
       discordTitleBar = true;
+      disableMinSize = true;
+      hardwareAcceleration = true;
       minimizeToTray = true;
       tray = true;
-      hardwareAcceleration = true;
-      discordBranch = "stable";
-      # spellCheckLanguages = ["fr-FR" "en-US"];
+
+      spellCheckLanguages = ["fr-FR" "en-US"];
     };
 
     vencord.themes = {
       catppuccin = "@import url('https://catppuccin.github.io/discord/dist/catppuccin-mocha-red.theme.css')";
     };
     vencord.settings = {
-      enabledThemes = ["catppuccin.css"];
       autoUpdate = true;
       autoUpdateNotification = false;
       notifyAboutUpdates = true;
+
+      enabledThemes = [
+        "caelestia.theme.css"
+        #"catppuccin.css"
+      ];
       useQuickCss = true;
+
       disableMinSize = true;
       plugins = {
-        MessageLogger = {
-          enabled = true;
-          ignoreSelf = true;
-        };
-        FakeNitro.enabled = true;
         AnonymiseFileNames.enabled = true;
+        # AlwaysAnimate.enabled = true;
+        BetterFolders = {
+          enabled = true;
+          closeOthers = true;
+          sidebar = false;
+          sidebarAnim = false;
+        };
         BetterSessions.enabled = true;
         BetterSettings.enabled = true;
         CallTimer.enabled = true;
         ClearURLs.enabled = true;
+        CopyFileContents.enabled = true;
+        CopyUserURLs.enabled = true;
         CustomRPC.enabled = true;
         # CustomIdle.enabled = true;
         DisableCallIdle = true;
+        FakeNitro.enabled = true;
         FavoriteEmojiFirst.enabled = true;
         FixImagesQuality.enabled = true;
         FixYoutubeEmbeds.enabled = true;
@@ -48,46 +60,42 @@
         GifPaste.enabled = true;
         ImageZoom = {
           enabled = true;
-          size = 245;
-          zoom = 2.70;
+          size = 300;
+          zoom = 2.50;
         };
-        # AlwaysAnimate.enabled = true;
         KeepCurrentChannel.enabled = true;
         LastFMRichPresence.enabled = true;
-        MessageLatency.enabled = true;
-        ReadAllNotificationsButton.enabled = true;
-        YoutubeAdblock.enabled = true;
-        VolumeBooster.enabled = true;
-        BetterFolders = {
+        MessageLogger = {
           enabled = true;
-          sidebar = false;
-          sidebarAnim = false;
-          closeOthers = true;
+          ignoreSelf = true;
         };
-        VoiceMessages.enabled = true;
-        CopyUserURLs.enabled = true;
-        Unindent.enabled = true;
-        NotTypingAnimation.enabled = true;
-        SilentTyping.enabled = true;
-        SpotifyControls.enabled = true;
-        OpenInApp.enabled = true;
-        ReviewDB.enabled = true;
+        MessageLatency.enabled = true;
+        MutualGroupDMs.enabled = true;
         NoDevtoolsWarning.enabled = true;
-        CopyFileContents.enabled = true;
+        NoTypingAnimation.enabled = true;
         NoUnblockToJump.enabled = true;
+        OpenInApp.enabled = true;
+        ReadAllNotificationsButton.enabled = true;
+        ReplaceGoogleSearch = {
+          enabled = true;
+          replacementEngine = "DuckDuckGo";
+        };
         ReverseImageSearch.enabled = true;
+        ReviewDB.enabled = true;
+        SilentTyping.enabled = true;
         ShowConnections.enabled = true;
         SortFriendRequests.enabled = true;
+        SpotifyControls.enabled = true;
+        ThemeAttributes.enabled = true;
         Translate.enabled = true;
+        Unindent.enabled = true;
         ValidReply.enabled = true;
         ValidUser.enabled = true;
         UserVoiceShow.enabled = true;
-        /*
-           RelplaceGoogleSearch = {
-          enabled = true;
-
-        };
-        */
+        ViewIcons.enabled = true;
+        VoiceMessages.enabled = true;
+        VolumeBooster.enabled = true;
+        YoutubeAdblock.enabled = true;
       };
     };
   };
