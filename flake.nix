86c1a5e2eec6ecff72db6322a45d12b34d8309f0
fe2-Nyxar <nyxar49@gmail.com>
2026-09-12 20:03:20 +0100

    update flake inputs

--- flake.nix
+++ flake.nix
@@ -36,18 +36,6 @@
       url = "github:Gerg-L/spicetify-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    /*
-         wallpapers = {
-        url = "github:42willow/wallpapers";
-        inputs.nixpkgs.follows = "nixpkgs";
-      };
-    */
-    /*
-         ags = {
-        url = "github:aylur/ags";
-        inputs.nixpkgs.follows = "nixpkgs";
-      };
-    */
     nur = {
       url = "github:nix-community/NUR";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -72,6 +60,18 @@
       url = "github:karaolidis/nix-obsidian-extensions";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    /*
+         wallpapers = {
+        url = "github:42willow/wallpapers";
+        inputs.nixpkgs.follows = "nixpkgs";
+      };
+    */
+    /*
+         ags = {
+        url = "github:aylur/ags";
+        inputs.nixpkgs.follows = "nixpkgs";
+      };
+    */
   };
 
   outputs =
