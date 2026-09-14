6bc8f212c4887b33fbebe91995e8457c009e1783
fe2-Nyxar <nyxar49@gmail.com>
2026-09-14 21:22:12 +0100

    add serverless SSH client

--- home/modules/services/ssh.nix
+++ home/modules/services/ssh.nix
@@ -1,32 +1,44 @@
+{ config, lib, ... }:
 {
   programs.ssh = {
     enable = true;
     enableDefaultConfig = false;
-    settings."*" = {
-      forwardAgent = false;
-      addKeysToAgent = "no";
-      compression = true;
-      serverAliveInterval = 0;
-      serverAliveCountMax = 3;
-      hashKnownHosts = true;
-      userKnownHostsFile = "~/.ssh/known_hosts";
-      controlMaster = "no";
-      controlPath = "~/.ssh/master-%r@%n:%p";
-      controlPersist = "no";
-    };
-    settings."github.com" = {
-      Hostname = "github.com";
-      User = "git";
-      IdentityFile = "~/.ssh/github";
-      IdentitiesOnly = true;
-      AddKeysToAgent = true;
-    };
-    settings."gitlab.com" = {
-      Hostname = "gitlab.com";
-      User = "git";
-      IdentityFile = "~/.ssh/github";
-      IdentitiesOnly = true;
-      AddKeysToAgent = true;
+    settings = {
+      "*" = {
+        forwardAgent = false;
+        addKeysToAgent = "no";
+        compression = true;
+        serverAliveInterval = 0;
+        serverAliveCountMax = 3;
+        hashKnownHosts = true;
+        userKnownHostsFile = "~/.ssh/known_hosts";
+        controlMaster = "no";
+        controlPath = "~/.ssh/master-%r@%n:%p";
+        controlPersist = "no";
+      };
+      "github.com" = {
+        Hostname = "github.com";
+        User = "git";
+        IdentityFile = "~/.ssh/github";
+        IdentitiesOnly = true;
+        AddKeysToAgent = true;
+      };
+      "gitlab.com" = {
+        Hostname = "gitlab.com";
+        User = "git";
+        IdentityFile = "~/.ssh/github";
+        IdentitiesOnly = true;
+        AddKeysToAgent = true;
+      };
+    }
+    // lib.optionalAttrs (config.home.username == "nyxar") {
+      serverless = {
+        Hostname = "serverless";
+        User = "baryon";
+        IdentityFile = "~/.ssh/serverless";
+        IdentitiesOnly = true;
+        AddKeysToAgent = true;
+      };
     };
   };
 }
